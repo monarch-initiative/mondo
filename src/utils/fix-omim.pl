97e6a40e411d259dae4ccf5560b6af6b653cb5bb
cmungall <cjm@berkeleybop.org>
2018-07-02 10:34:56 -0700

    temp hack for https://github.com/monarch-initiative/dipper/issues/600

---
+++ src/utils/fix-omim.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+while(<>) {
+    s@ EXACT @ RELATED @;
+    if (m@synonym: "(.*), formerly" \S+@i) {
+        print "synonym: \"$1\" RELATED []\n";
+    }
+    s@OMIM:PS@OMIMPS:@g;
+    if (m@is_a: OMIMPS:(\d+),PS(\d+)@) {
+        $_ = "is_a: OMIMPS:$1\nis_a: OMIMPS:$2\n";;
+    }
+    print $_;
+    
+}
