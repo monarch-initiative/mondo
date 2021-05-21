2a089e03120aaa6109a22a768be31a8c220d4d28
Nico Matentzoglu <nicolas.matentzoglu@gmail.com>
2021-05-21 16:46:00 +0300

    Commit all updates sssom files

--- src/scripts/split_sssom_by_source.py
+++ src/scripts/split_sssom_by_source.py
@@ -14,9 +14,9 @@ import pandas as pd
 from argparse import ArgumentParser
 import yaml
 
-from sssom.datamodel_util import read_pandas, read_metadata
-from sssom.parsers import from_dataframe
-from sssom.writers import write_tsv
+from sssom.datamodel_util import read_pandas
+from sssom.parsers import from_dataframe, split_dataframe
+from sssom.writers import write_tsv, write_tsvs
 
 import logging
 
@@ -30,15 +30,6 @@ sssom_file=args.sssom_tsv_path
 metadata_file=args.sssom_metadata_path
 mapping_dir=args.tsv_out_path
 
-meta, curie_map=read_metadata(metadata_file)
-meta["mapping_date"]=datetime.now().strftime("%Y-%m-%d")
-df=read_pandas(sssom_file)
-df["match_type"]="HumanCurated"
-
-subject_prefixes=set(df['subject_id'].str.split(':', 1, expand=True)[0])
-object_prefixes=set(df['object_id'].str.split(':', 1, expand=True)[0])
-#object_prefixes.remove("MONDO")
-relations=set(df['predicate_id'])
 
 def reconcile_curie(pre_orig):
     if re.match(r".*__[0-9]$", pre_orig):
@@ -59,24 +50,42 @@ def replace_temporary_prefixes(prefixes, df):
             df['subject_id'] = df['subject_id'].str.replace(pre_orig+":",pre+":")
     return df
 
+def read_metadata(filename):
+    """
+    Reading metadata file (yaml) that is supplied separately from a tsv
+    :param filename: location of file
+    :return: two objects, a metadata and a curie_map object
+    """
+    meta = {}
+    curie_map = {}
+    with open(filename, 'r') as stream:
+        try:
+            m = yaml.safe_load(stream)
+            if "curie_map" in m:
+                curie_map = m['curie_map']
+            m.pop('curie_map', None)
+            meta = m
+        except yaml.YAMLError as exc:
+            print(exc)
+    return meta, curie_map
+
+meta, curie_map=read_metadata(metadata_file)
+df=read_pandas(sssom_file)
+df["match_type"]="HumanCurated"
+
+subject_prefixes=set(df['subject_id'].str.split(':', 1, expand=True)[0])
+object_prefixes=set(df['object_id'].str.split(':', 1, expand=True)[0])
+relations=set(df['predicate_id'])
+
 df = replace_temporary_prefixes(subject_prefixes, df)
 df = replace_temporary_prefixes(object_prefixes, df)
+msdf = from_dataframe(df, curie_map=curie_map, meta=meta['global_metadata'])
 
-for pre_subj in subject_prefixes:
-    for pre_obj in object_prefixes:
-        for rel in relations:
-            relpre=rel.split(":")[0]
-            relppost=rel.split(":")[1]
-            sssom_file=os.path.join(mapping_dir,f"{pre_subj.lower()}_{relppost.lower()}_{pre_obj.lower()}.sssom.tsv")
-    
-            dfs=df[(df['subject_id'].str.startswith(pre_subj+":")) 
-                   & (df['predicate_id'] == rel) 
-                   & (df['object_id'].str.startswith(pre_obj+":"))]
-            if pre_subj in curie_map and pre_obj in curie_map and len(dfs)>0:
-                cm = {pre_subj: curie_map[pre_subj], pre_obj: curie_map[pre_obj], relpre: curie_map[relpre]}
-                msdf=from_dataframe(dfs,curie_map=cm,meta=meta)
-                write_tsv(msdf=msdf,filename=sssom_file)
-                print(f"Writing {sssom_file} complete!")
-            else:
-                print(f"Not creating {sssom_file} because there is a missing prefix ({pre_subj}, {pre_obj}), or no matches ({len(dfs)} matches found)")
-    
+splitted = split_dataframe(msdf)
+for msdf in splitted:
+    m = msdf.meta
+    for source_metadata in meta['source_metadata']:
+        if 'metadata' in source_metadata:
+            for item in source_metadata['metadata']:
+                m[item]=source_metadata['metadata'][item]
+write_tsvs(splitted,mapping_dir)
