1255b9c724b49912407411ac3882d260abe7de0d
Nico Matentzoglu <nicolas.matentzoglu@gmail.com>
2022-01-25 13:29:05 +0200

    Finish obsoletion schema

--- src/scripts/merge_release_diff.py
+++ src/scripts/merge_release_diff.py
@@ -7,6 +7,8 @@ parser.add_argument('report_last_release', type=str,
                     help='A TSV file containing the stats for the last release')
 parser.add_argument('report_current_release', type=str,
                     help='A TSV file containing the stats for the last release')
+parser.add_argument('report_obsoletion_candidates', type=str,
+                    help='A TSV file containing the current obsoletion candidates')
 parser.add_argument('output', type=str,
                     help='A TSV path for the output of changed terms')
 parser.add_argument('output_new', type=str,
@@ -15,6 +17,7 @@ parser.add_argument('output_new', type=str,
 args = parser.parse_args()
 report_last_release=args.report_last_release
 report_current_release=args.report_current_release
+report_current_obsoletion_candidates=args.report_obsoletion_candidates
 output=args.output
 output_new=args.output_new
 
@@ -40,6 +43,7 @@ def format_new_table(df):
 
 df_report_last_release=load_report_data(report_last_release,'old')
 df_report_current_release=load_report_data(report_current_release,'new')
+df_report_current_obsoletion_candidates = pd.read_csv(report_current_obsoletion_candidates, sep='\t')
 
 last_ids = df_report_last_release['mondo_id'].unique()
 current_ids = df_report_current_release['mondo_id'].unique()
@@ -65,8 +69,75 @@ df_report_new = df_report_new_wide.copy()
 
 df_report_changed=df_report[~df_report['mondo_id'].isin(new_terms)].copy()
 
+# Change empty values to false and format URLs better without the <> around them
+df_report_new[['obsolete', 'obsoletion_candidate']] = df_report_new[['obsolete', 'obsoletion_candidate']].replace(r'^\s*$', "", regex=True)
+df_report_current_obsoletion_candidates[['issue']] = df_report_current_obsoletion_candidates[['issue']].replace(r'[<>]', "", regex=True)
+
 df_report_new.to_csv(output_new, sep="\t",index=False)
 df_report_changed.to_csv(output, sep="\t",index=False)
+df_report_current_obsoletion_candidates.to_csv(report_current_obsoletion_candidates, sep="\t",index=False)
+
+print("----------------START LOG: DO NOT INCLUDE ME IN RELEASE NOTES----------------------")
+print("")
+print("QC: obsoletion schema integrity checks")
+print("")
+print("Checkin table: Obsoletion candidates table")
+print("")
+if not df_report_current_obsoletion_candidates['mondo_id'].is_unique:
+    print("MONDO IDs in obsoletion candidate table are not unique.")
+    print(pd.concat(g for _, g in df_report_current_obsoletion_candidates.groupby("mondo_id") if len(g) > 1))
+    print("")
+
+if len(df_report_current_obsoletion_candidates[df_report_current_obsoletion_candidates['obsoletion_date'] == ''])>0:
+    print("Date is not set for some obsoletion candidates.")
+    print(df_report_current_obsoletion_candidates[df_report_current_obsoletion_candidates['obsoletion_date'] == ''])
+    print("")
+
+if len(df_report_current_obsoletion_candidates[df_report_current_obsoletion_candidates['issue'] == ''])>0:
+    print("Issue is not set for some obsoletion candidates.")
+    print(df_report_current_obsoletion_candidates[df_report_current_obsoletion_candidates['issue'] == ''])
+    print("")
+
+
+print("Table: Changed terms table")
+print("")
+dfx = df_report_changed.groupby(['mondo_id', 'property']).size().reset_index(name='freq')
+
+if len(dfx[dfx['freq']>1]):
+    print("MONDO IDs in changed term table are not unique.")
+    print(dfx[dfx['freq']>1])
+    print("")
+
+print("Table: New terms table")
+print("")
+dfx = df_report_changed.groupby(['mondo_id', 'property']).size().reset_index(name='freq')
+
+if not df_report_new['mondo_id'].is_unique:
+    print("MONDO IDs in new terms table are not unique.")
+    print(pd.concat(g for _, g in df_report_new.groupby("mondo_id") if len(g) > 1))
+    print("")
+
+mondoidsnew = df_report_new['mondo_id'].unique()
+mondoidschanged = df_report_changed['mondo_id'].unique()
+mondoidsobsoletioncandidate = df_report_current_obsoletion_candidates['mondo_id'].unique()
+
+new_and_changed = list(set(mondoidsnew) & set(mondoidschanged))
+
+if new_and_changed:
+    print("Mondo IDs in new term table appear in changed tables as well!")
+    print(new_and_changed)
+    print("")
+
+new_and_obsoletion_candidate = list(set(mondoidsnew) & set(mondoidsobsoletioncandidate))
+
+if new_and_obsoletion_candidate:
+    print("Mondo IDs in new term table appear in obsoletion candidate table as well!")
+    print(new_and_obsoletion_candidate)
+    print("")
+
+print("----------------END LOG: DO NOT INCLUDE ME IN RELEASE NOTES----------------------")
+
+# Preparing data frames for printing.
 
 df_changed_labels = df_report_changed[(df_report_changed['property']=='label') & ~(df_report_changed['mondo_id'].isin(obsoleted_terms))]
 df_changed_definitions = df_report_changed[(df_report_changed['property']=='definition') & ~(df_report_changed['mondo_id'].isin(obsoleted_terms))]
@@ -74,13 +145,13 @@ df_obsolete_terms = df_report_changed[(df_report_changed['property']=='obsolete'
 df_obsolete_candidates = df_report_changed[(df_report_changed['property']=='obsoletion_candidate') & (df_report_changed['value_old']!=True) & ~(df_report_changed['mondo_id'].isin(obsoleted_terms))]
 df_old_obsolete_candidates = df_report_changed[(df_report_changed['property']=='obsoletion_candidate') & (df_report_changed['value_old']==True) & ~(df_report_changed['mondo_id'].isin(obsoleted_terms))]
 
-
 ct_new_terms = len(df_report_new['mondo_id'].unique())
 ct_changed_labels = len(df_changed_labels['mondo_id'].unique())
 ct_changed_definitions = len(df_changed_definitions['mondo_id'].unique())
 ct_obsolete_terms = len(df_obsolete_terms['mondo_id'].unique())
 ct_obsolete_candidates = len(df_obsolete_candidates['mondo_id'].unique())
 ct_old_obsolete_candidates = len(df_old_obsolete_candidates['mondo_id'].unique())
+
 
 print("## Overview:")
 print("")
@@ -157,4 +228,3 @@ if len(df_old_obsolete_candidates)>0:
     print(df_obsolete_candidates[['Mondo ID','Label']].to_markdown(index=False))
 else:
     print("No changes.")
-
