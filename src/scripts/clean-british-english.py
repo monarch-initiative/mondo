4804e9be1aaddd93733617e588b51ca9ffe4dee2
Nico Matentzoglu <nicolas.matentzoglu@gmail.com>
2023-08-11 18:23:26 +0300

    Update clean-british-english.py

--- src/scripts/clean-british-english.py
+++ src/scripts/clean-british-english.py
@@ -13,12 +13,21 @@ with open(dictionary, 'r') as f:
     replacements = {rows[0]: rows[1] for rows in reader}
 
 with open(input, 'r') as f:
-    content = f.read()
+    text = f.read()
 
-for old, new in replacements.items():
-    if old=="disc":
-        old="disc([^a-zA-Z]|$)"
-    content = re.sub(old, new, content)
+def replace_words_from_dict(text, word_dict):
+    """Replace words in text based on dictionary."""
+
+    def replacement(match):
+        word = match.group(0)
+        clean_word = word.strip('.,!?()[]{}":;')
+        return word.replace(clean_word, word_dict.get(clean_word, clean_word))
+
+    # This regex will capture words and preserve punctuations.
+    return re.sub(r'\b[\w\'-]+\b', replacement, text)
+
+
+content = replace_words_from_dict(text, replacements)
 
 with open(input, 'w') as f:
     f.write(content)
