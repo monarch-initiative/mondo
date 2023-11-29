b74ff38bfe6edf5c70a4a9346acab23fe3dd747b
Harshad Hegde <hegdehb@gmail.com>
2023-11-29 16:01:12 -0600

    new run

--- src/scripts/split_sssom_by_source.py
+++ src/scripts/split_sssom_by_source.py
@@ -14,9 +14,10 @@ import pandas as pd
 from argparse import ArgumentParser
 import yaml
 
-from sssom.util import read_pandas, sort_df_rows_columns
-from sssom.parsers import from_sssom_dataframe, split_dataframe_by_prefix
-from sssom.writers import write_table, write_tables
+from sssom.util import sort_df_rows_columns
+from sssom.parsers import from_sssom_dataframe, split_dataframe_by_prefix, parse_sssom_table
+from sssom.writers import write_tables
+from sssom.constants import SEMAPV
 
 import logging
 
@@ -31,24 +32,24 @@ metadata_file=args.sssom_metadata_path
 mapping_dir=args.tsv_out_path
 
 
-def reconcile_curie(pre_orig):
-    if re.match(r".*__[0-9]$", pre_orig):
-        print(f"{pre_orig} non standard prefix, reconciling")
-        pre = pre_orig[:-3]
-    else:
-        pre = pre_orig
-    return pre
+# def reconcile_curie(pre_orig):
+#     if re.match(r".*__[0-9]$", pre_orig):
+#         print(f"{pre_orig} non standard prefix, reconciling")
+#         pre = pre_orig[:-3]
+#     else:
+#         pre = pre_orig
+#     return pre
 
 
-def replace_temporary_prefixes(prefixes, df):
-    for pre_orig in prefixes:
-        pre = reconcile_curie(pre_orig)
-        if pre!=pre_orig:
-            print(f"Replacing temporary curie {pre_orig} with {pre}")
-            logging.warning(f"Replacing temporary curie {pre_orig} with {pre}")
-            df['object_id'] = df['object_id'].str.replace(pre_orig+":",pre+":")
-            df['subject_id'] = df['subject_id'].str.replace(pre_orig+":",pre+":")
-    return df
+# def replace_temporary_prefixes(prefixes, df):
+#     for pre_orig in prefixes:
+#         pre = reconcile_curie(pre_orig)
+#         if pre!=pre_orig:
+#             print(f"Replacing temporary curie {pre_orig} with {pre}")
+#             logging.warning(f"Replacing temporary curie {pre_orig} with {pre}")
+#             df['object_id'] = df['object_id'].str.replace(pre_orig+":",pre+":")
+#             df['subject_id'] = df['subject_id'].str.replace(pre_orig+":",pre+":")
+#     return df
 
 def read_metadata(filename):
     """
@@ -70,24 +71,23 @@ def read_metadata(filename):
     return meta, curie_map
 
 meta, curie_map=read_metadata(metadata_file)
-df=read_pandas(sssom_file)
-df["mapping_justification"]="semapv:ManualMappingCuration"
-
+msdf_main=parse_sssom_table(sssom_file)
+msdf_main.df["mapping_justification"]=SEMAPV.ManualMappingCuration.value
 subject_prefixes_allowed = meta['subject_prefixes']
 relations_allowed = meta['relations']
 
-subject_prefixes=set(df['subject_id'].str.split(pat=':', n=1, expand=True)[0])
-object_prefixes=set(df['object_id'].str.split(pat=':',n= 1, expand=True)[0])
-relations=set(df['predicate_id'])
+# subject_prefixes=set(df['subject_id'].str.split(pat=':', n=1, expand=True)[0])
+object_prefixes=set(msdf_main.df['object_id'].str.split(pat=':',n= 1, expand=True)[0])
+# relations=set(df['predicate_id'])
 
-df = replace_temporary_prefixes(subject_prefixes, df)
-df = replace_temporary_prefixes(object_prefixes, df)
-global_metadata=meta['global_metadata']
+# df = replace_temporary_prefixes(subject_prefixes, df)
+# df = replace_temporary_prefixes(object_prefixes, df)
+# global_metadata=meta['global_metadata']
 
-msdf = from_sssom_dataframe(df, prefix_map=curie_map, meta=global_metadata)
+# msdf = from_sssom_dataframe(df, prefix_map=curie_map, meta=global_metadata)
 today = datetime.today().strftime('%Y-%m-%d')
 
-splitted = split_dataframe_by_prefix(msdf,subject_prefixes_allowed,object_prefixes, relations_allowed)
+splitted = split_dataframe_by_prefix(msdf_main,subject_prefixes_allowed,object_prefixes, relations_allowed)
 for msdf in splitted:
     fromS = msdf.split("_")[0].upper()
     toS = msdf.split("_")[2].upper()
