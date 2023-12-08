78a619f13a29b71294cd7ee0b69a8722237524d3
Harshad <hrshdhgd@users.noreply.github.com>
2023-12-07 19:04:50 -0600

    Just ran `black` and ruff --fix (#6987)

--- src/scripts/dosdp-matches-tags.py
+++ src/scripts/dosdp-matches-tags.py
@@ -9,16 +9,18 @@ import pandas as pd
 from pathlib import Path
 from argparse import ArgumentParser
 
-mondo_patterns_iri="http://purl.obolibrary.org/obo/mondo/patterns/"
+mondo_patterns_iri = "http://purl.obolibrary.org/obo/mondo/patterns/"
+
 
 def get_dataframe(tsv):
     try:
-        df = pd.read_csv(tsv,sep="\t")
-        df["pattern"]=mondo_patterns_iri+Path(tsv).stem+".yaml"
-        return df[["defined_class","pattern"]]
+        df = pd.read_csv(tsv, sep="\t")
+        df["pattern"] = mondo_patterns_iri + Path(tsv).stem + ".yaml"
+        return df[["defined_class", "pattern"]]
     except pd.errors.EmptyDataError:
         print("WARNING! ", tsv, " is empty and has been skipped.")
-    return pd.DataFrame(columns=['defined_class','pattern'])
+    return pd.DataFrame(columns=["defined_class", "pattern"])
+
 
 parser = ArgumentParser()
 parser.add_argument(
@@ -28,11 +30,18 @@ parser.add_argument(
     dest="dosdp_tsvs",
     help="The set of mapping suggestions to be merged.",
 )
-parser.add_argument("-o", "--output", dest="tsv_out_path", help="Output file", metavar="FILE")
+parser.add_argument(
+    "-o", "--output", dest="tsv_out_path", help="Output file", metavar="FILE"
+)
 args = parser.parse_args()
 
 merge = []
-merge.append(pd.DataFrame({'defined_class':  ['ID'], 'pattern': ['AI dc:conformsTo']},columns=['defined_class','pattern']))
+merge.append(
+    pd.DataFrame(
+        {"defined_class": ["ID"], "pattern": ["AI dc:conformsTo"]},
+        columns=["defined_class", "pattern"],
+    )
+)
 merge.extend([get_dataframe(f) for f in args.dosdp_tsvs])
 
 df = pd.concat(merge)
@@ -40,5 +49,4 @@ df.reset_index(drop=True, inplace=True)
 
 print(df.head())
 
-df.to_csv(args.tsv_out_path,sep="\t",index=False)
-
+df.to_csv(args.tsv_out_path, sep="\t", index=False)
